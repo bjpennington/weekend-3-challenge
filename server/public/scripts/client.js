024621c7beb7bf40fe032499b6de1cadf7c786fe
BJ Pennington <bjpennington112@gmail.com>
2018-07-07 15:35:41 -0500

    set up basic angular and DOM interaction

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,9 +2,18 @@ console.log('js');
 
 const app = angular.module('ToDoApp', []);
 
+let testArray = [];
+
 app.controller('ToDoController', [function () {
     const self = this;
-    self.submitToDo = function () {
+
+
+
+    self.submitToDo = function (newToDo) {
         console.log('to do added');
+        console.log(newToDo);
+        testArray.push(newToDo);
     }
+
+    self.toDos = testArray;
 }]);
